a8662e3f5e41ed9aca7bb13c1cd5a12f29739792
DanielJaque <91282996+DanielJaque@users.noreply.github.com>
2021-09-29 19:50:52 -0300

    Create ASCII.c

---
+++ programación5/ASCII.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    char caracter;
+printf("Buen día, por favor ingrese un caracter\n");
+scanf("%c", &caracter);
+printf("El equivalente de %c es: %d\n", caracter,caracter);
+return 0;
+}
